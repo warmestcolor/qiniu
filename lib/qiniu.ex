7a501c808b1282155fcd0721de729babfa7e3110
neo <nexoimy@gmail.com>
2018-10-23 12:01:33 +0800

    update qiniu up-z2

--- lib/qiniu.ex
+++ lib/qiniu.ex
@@ -27,11 +27,16 @@ defmodule Qiniu do
     [
       user_agent:       "QiniuElixir/#{System.version}",
       content_type:     "application/x-www-form-urlencoded",
-      up_host:          "http://up.qiniu.com",
+      up_host:          "http://up-z2.qiniup.com",
       rs_host:          "http://rs.qiniu.com",
       rsf_host:         "http://rsf.qbox.me",
       io_host:          "http://iovip.qbox.me",
       api_host:         "http://api.qiniu.com"
+      # up_host:          "http://up.qiniu.com",
+      # rs_host:          "http://rs.qiniu.com",
+      # rsf_host:         "http://rsf.qbox.me",
+      # io_host:          "http://iovip.qbox.me",
+      # api_host:         "http://api.qiniu.com"
     ]
   end
 end
